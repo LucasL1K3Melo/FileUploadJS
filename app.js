cfe7442333a3a6b753ff91d8db98a51a9c01b656
LucasL1K3Melo <lucas.matheus0104@gmail.com>
2022-06-29 23:38:46 -0300

    bugs fix

--- app.js
+++ app.js
@@ -21,22 +21,24 @@ fileInput.onchange = ({target}) => {
 }
 
 function uploadFile(name){
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/upload.php");
-    xhr.upload.addEventListener("progress", ({loaded, total}) => {
-        let fileLoaded = Math.floor((loaded / total) * 100);
+    let xhr = new XMLHttpRequest(); // Creating a new XMR Object (Ajax):
+    xhr.open("POST", "php/upload.php"); // Sending post request to the specified URL:
+    xhr.upload.addEventListener("progress", ({loaded, total}) => { // File event progress event
+        let fileLoaded = Math.floor((loaded / total) * 100); // Getting porcentage of loaded file size:
+        let fileTotal = Math.floor(total / 1000); // Getting total file size in kb from bytes:
         let fileSize;
 
+        // If file size is less than 1024 then add only KB else convert this KB into MB:
         (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB"; 
         let progressHTML = `<li class="row">
                                 <i class="fas fa-file-alt"></i>
                                 <div class="content">
                                     <div class="details">
                                         <span class="name"> ${name} * Uploading</span>
-                                        <span class="percent"> ${fileLoaded} </span>
+                                        <span class="percent"> ${fileLoaded}%</span>
                                     </div>
                                     <div class="progress-bar">
-                                        <div class="progress" style="width: ${fileLoded} %"></div>
+                                        <div class="progress" style="width: ${fileLoaded} %"></div>
                                     </div>
                                 </div>
                             </li>`;
@@ -46,18 +48,20 @@ function uploadFile(name){
 
         if(loaded == total){
             progressArea.innerHTML = "";
+
             let uploadedHTML = `<li class="row">
                                     <div class="content upload">
                                         <i class="fas fa-file-alt"></i>
                                         <div class="details">
-                                            <span class="name"> ${name} * Uploaded</span>
+                                            <span class="name"> ${name} Uploaded</span>
                                             <span class="size"> ${fileSize} </span>
                                         </div>
                                     </div>
                                     <i class="fas fa-check"></i>
                                 </li>`;
+
             uploadedArea.classList.remove("onprogress");
-            uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+            uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); // R
 
         }
     
